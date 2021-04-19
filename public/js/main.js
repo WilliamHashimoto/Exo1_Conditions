cd1d79ab5cbe7ca60b981f933956f8adc173bf0c
WilliamHashimoto <79861381+WilliamHashimoto@users.noreply.github.com>
2021-04-19 16:54:14 +0200

    fini

---
+++ public/js/main.js
@@ -0,0 +1,74 @@
+// //1
+// if (1 == "1") {
+//     console.log("Vérifié");
+// }
+// else {
+//     console.log("Faux");
+// }
+
+// //2
+// if (1 === "1") {
+//     console.log("Vérifié");
+// }
+// else {
+//     console.log("Faux");
+// }
+
+// //3
+// let nom = prompt("Quel est ton prénom?");
+// if (nom.length < 5) {
+//     alert("Ton nom est trop court")
+// }
+// else {
+//     alert(`Salut ${nom}`);
+// }
+
+// //4
+// let add = prompt("5 + 5 = ?");
+
+// if (add == 10) {
+//     alert("Félicitations");
+// }
+// else {
+//     alert("C'est faux");
+// }
+
+// //5 
+// let mult = prompt("2 * 10 = ?");
+// if (mult == 20) {
+//     alert("Félicitation. La réponse est bien 20");
+// }
+// else {
+//     alert("C'est dommage, tu n'étais qu'à " + Math.abs(20 - mult) + " unités de la bonne réponse");
+// }
+
+// //6
+// let tab = [];
+// let prompt1 = prompt("Insérer un élément: ");
+// tab.push(prompt1);
+// let prompt2 = prompt("Insérer un élément: ");
+// tab.push(prompt2);
+// let prompt3 = prompt("Insérer un élément: ");
+// tab.push(prompt3);
+// alert(tab);
+
+//7
+// let grandNombres = [];
+// let petitNombres = [];
+// let elem1 = prompt("entrez un chiffre");
+// let elem2 = prompt("entrez un chiffre");
+// if (elem1 >= 12) {
+//     grandNombres.push(elem1);
+// }
+// else {
+//     petitNombres.push(elem1);
+// }
+// if (elem2 >= 12) {
+//     grandNombres.push(elem2);
+// }
+// else {
+//     petitNombres.push(elem2);
+// }
+// alert(`petitNombres = ${petitNombres} ` + `grandNombres = ${grandNombres}`);
+
+//8
